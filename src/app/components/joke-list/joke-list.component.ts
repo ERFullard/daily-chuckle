06953d1191ea16dfadab9c17e8edea5ebb447f40
Edwin Fullard <edwin.fullard@rabobank.nl>
2025-05-14 19:47:17 +0200

    feat: add list component and animations

---
+++ src/app/components/joke-list/joke-list.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input} from '@angular/core';
+import {JokeListItemComponent} from '../joke-list-item/joke-list-item.component';
+import {NgForOf} from '@angular/common';
+import {JokeItem} from '../../shared/types';
+import {
+  animate, query,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-joke-list',
+  imports: [
+    JokeListItemComponent,
+    NgForOf
+  ],
+  templateUrl: './joke-list.component.html',
+  styleUrl: './joke-list.component.css',
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ overflow: 'hidden', height: '0', transform: 'translateX(100%)', opacity: '0' }),
+        animate('150ms', style({ height: '*'})),
+        animate('300ms', style({ transform: 'translateX(0)', opacity: 1})),
+      ]),
+      transition(':leave', [
+        style({ overflow: 'hidden'}),
+        animate('300ms', style({ transform: 'translateX(-100%)', opacity: 0})),
+        animate('150ms', style({ height: '0'})),
+      ]),
+    ])
+  ]
+})
+export class JokeListComponent {
+  @Input()
+  items!: JokeItem[];
+
+  trackByFn(index: number, item: JokeItem) {
+    return item.id;
+  }
+}
